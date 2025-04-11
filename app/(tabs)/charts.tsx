37ea011703cfc58498c6c89727ab02ba7422bfea
Mathias6120 <93884797+mbartolas7@users.noreply.github.com>
2025-04-12 01:34:16 +0200

    12/04/25

--- app/(tabs)/charts.tsx
+++ app/(tabs)/charts.tsx
@@ -55,16 +55,16 @@ const date_picker_mode_data = [
     id: "single",
   },
   {
-    name: "Période",
-    id: "range",
-  },
-  {
     name: "Mois",
     id: "month",
   },
   {
     name: "Année",
     id: "year",
+  },
+  {
+    name: "Période",
+    id: "range",
   },
 ];
 
@@ -86,12 +86,12 @@ export default function ChartsScreen() {
   const [selectedDate, setSelectedDate] = useState<DateType>(today);
   const [selectedStartDate, setSelectedStartDate] = useState<DateType>();
   const [selectedEndDate, setSelectedEndDate] = useState<DateType>();
+  const [selectedMonth, setSelectedMonth] = useState<number>(today.getMonth());
+  const [selectedYear, setSelectedYear] = useState<number>(today.getFullYear());
 
   const [appliedDate, setAppliedDate] = useState<DateType>(today);
   const [appliedStartDate, setAppliedStartDate] = useState<DateType>(today);
   const [appliedEndDate, setAppliedEndDate] = useState<DateType>(today);
-
-  const [bottomSheetShown, setBottomSheetShown] = useState(false);
 
   const date_picker_sheet = useRef<BottomSheetModalRef>(null);
 
@@ -99,6 +99,7 @@ export default function ChartsScreen() {
 
   const [selectedType, setSelectedType] = useState<string>("global");
   const [selectedMode, setSelectedMode] = useState<string>("single");
+  const [appliedMode, setAppliedMode] = useState<string>("single");
 
   const chart_width = Dimensions.get("window").width - 30 - 30;
 
@@ -120,7 +121,7 @@ export default function ChartsScreen() {
   const getDateText = () => {
     let text = "";
 
-    switch (selectedMode) {
+    switch (appliedMode) {
       case "single":
         if (moment(appliedDate).isSame(today, "day")) {
           text += "Ajourd'hui, ";
@@ -157,6 +158,16 @@ export default function ChartsScreen() {
           text += moment(appliedEndDate).format("D MMMM YYYY");
         }
         break;
+      case "month":
+        if (moment(appliedStartDate).isSame(today, "year")) {
+          text += moment(appliedStartDate).format("MMMM");
+        } else {
+          text += moment(appliedStartDate).format("MMMM YYYY");
+        }
+        break;
+      case "year":
+        text += moment(appliedStartDate).format("YYYY");
+        break;
       default:
         break;
     }
@@ -164,25 +175,59 @@ export default function ChartsScreen() {
     return text;
   };
 
-  const handleSelectDate = (date_data: object) => {
-    if (selectedMode == "range") {
+  const handleSelectDate = (date_data: object | number) => {
+    if (selectedMode == "single" && typeof date_data == "object") {
+      const { date } = date_data;
+      console.log(date);
+      setSelectedDate(date);
+    } else if (selectedMode == "range" && typeof date_data == "object") {
       const { startDate, endDate } = date_data;
       setSelectedStartDate(startDate);
       setSelectedEndDate(endDate);
-    } else {
-      const { date } = date_data;
-      setSelectedDate(date);
+    } else if (selectedMode == "month" && typeof date_data == "number") {
+      // Number == year || index of month (january = 0)
+      const is_month = date_data >= 0 && date_data <= 11;
+      const is_year = date_data > 11;
+
+      if (is_month) {
+        setSelectedMonth(date_data);
+      } else if (is_year) {
+        setSelectedYear(date_data);
+      }
+    } else if (selectedMode == "year" && typeof date_data == "number") {
+      console.log(date_data);
+      setSelectedYear(date_data);
     }
   };
 
   const handleConfirmDate = () => {
-    setAppliedDate(selectedDate);
-    setAppliedStartDate(selectedStartDate);
-    setAppliedEndDate(selectedEndDate);
+    switch (selectedMode) {
+      case "single":
+        setAppliedDate(selectedDate);
+        break;
+      case "range":
+        setAppliedStartDate(selectedStartDate);
+        setAppliedEndDate(selectedEndDate);
+        break;
+      case "month":
+        setAppliedStartDate(new Date(selectedYear, selectedMonth, 1));
+        setAppliedEndDate(new Date(selectedYear, selectedMonth + 1, 0));
+        break;
+      case "year":
+        setAppliedStartDate(new Date(selectedYear, 0, 1));
+        setAppliedEndDate(new Date(selectedYear, 11, 31));
+        break;
+      default:
+        break;
+    }
+
+    setAppliedMode(selectedMode);
+
     date_picker_sheet.current?.toggleShowBottomSheet();
   };
 
-  const handleApplyMode = (mode: string) => {
+  const handleSelectMode = (mode: string) => {
+    console.log("selected mode : " + mode);
     setSelectedMode(mode);
   };
 
@@ -194,7 +239,9 @@ export default function ChartsScreen() {
         // header: { display: "none" },
         weekday: { display: "none" },
         weekdays: { display: "none" },
-        months: { marginTop: -100 },
+        months: {
+          marginTop: 0,
+        },
       };
     }
     return {};
@@ -202,7 +249,17 @@ export default function ChartsScreen() {
 
   const year_selector_styles = () => {
     if (selectedMode == "year") {
-      return {};
+      return {
+        day: { display: "none" },
+        days: { display: "none" },
+        // header: { display: "none" },
+        weekday: { display: "none" },
+        weekdays: { display: "none" },
+        months: {
+          marginTop: 0,
+        },
+        month_selector: { display: "none" },
+      };
     }
     return {};
   };
@@ -264,7 +321,15 @@ export default function ChartsScreen() {
             onPress={() => date_picker_sheet.current?.toggleShowBottomSheet()}
             style={styles.main_date}
           >
-            <Text style={[styles.main_date_text, { color: theme.dark_text }]}>
+            <Text
+              style={[
+                styles.main_date_text,
+                {
+                  color: theme.dark_text,
+                  textTransform: appliedMode == "month" ? "capitalize" : "none",
+                },
+              ]}
+            >
               {getDateText()}
             </Text>
             <ChevronDown color={theme.dark_text} />
@@ -314,7 +379,7 @@ export default function ChartsScreen() {
 
       <BottomSheetModalContainer
         title={"Date"}
-        snap_points={["50%", "75%"]}
+        snap_points={["75%"]}
         ref={date_picker_sheet}
         onAnimate={(actual_index, next_index) => {
           // index == -1 => close
@@ -325,7 +390,14 @@ export default function ChartsScreen() {
             setShowBottomTab(false);
           }
         }}
-        footer_dependencies={[selectedDate, selectedStartDate, selectedEndDate]}
+        footer_dependencies={[
+          selectedDate,
+          selectedStartDate,
+          selectedEndDate,
+          selectedYear,
+          selectedMonth,
+          selectedMode,
+        ]}
         footer={
           <ButtonContainer action={() => handleConfirmDate()}>
             <View
@@ -351,14 +423,19 @@ export default function ChartsScreen() {
         >
           <FilterList
             filters={date_picker_mode_data}
-            handleApplyFilter={handleApplyMode}
+            handleApplyFilter={handleSelectMode}
             default_filter={selectedMode}
           />
           <DateTimePicker
             mode={selectedMode == "range" ? "range" : "single"}
-            // mode="range"
-            date={selectedDate}
+            date={
+              selectedMode == "month" || selectedMode == "year"
+                ? new Date(selectedYear, selectedMonth, 1)
+                : selectedDate
+            }
             onChange={(date: object) => handleSelectDate(date)}
+            onMonthChange={(month: number) => handleSelectDate(month)}
+            onYearChange={(year: number) => handleSelectDate(year)}
             startDate={selectedStartDate}
             endDate={selectedEndDate}
             styles={{
@@ -372,13 +449,21 @@ export default function ChartsScreen() {
                 ...styles.date_selected_label,
                 color: theme.light_text,
               }, // Highlight the selected day label
+              header: styles.date_picker_header,
               month_label: styles.date_picker_text,
               month_selector_label: styles.date_picker_title,
+              year_label: styles.date_picker_text,
               year_selector_label: styles.date_picker_title,
               day_label: styles.date_picker_text,
+              selected_month: { backgroundColor: theme.tint },
+              selected_year: { backgroundColor: theme.tint },
+              year: { ...styles.date_picker_item, borderColor: theme.stroke },
+              month: { ...styles.date_picker_item, borderColor: theme.stroke },
+              day: { ...styles.date_picker_item, borderColor: theme.stroke },
               ...month_selector_styles(),
               ...year_selector_styles(),
             }}
+            style={styles.date_picker}
             firstDayOfWeek={1}
             locale="fr"
             timeZone="Europe/Brussels"
@@ -426,7 +511,6 @@ const styles = StyleSheet.create({
   main_date_text: {
     fontFamily: "Figtree-SemiBold",
     fontSize: 18,
-    letterSpacing: -0.4,
   },
 
   bottom_sheet: {
@@ -446,6 +530,12 @@ const styles = StyleSheet.create({
     // letterSpacing: -0.4,
   },
 
+  date_picker: {
+    marginTop: 15,
+  },
+  date_picker_header: {
+    marginBottom: 15,
+  },
   date_picker_today: {
     borderWidth: 2,
   },
@@ -460,6 +550,10 @@ const styles = StyleSheet.create({
     textTransform: "capitalize",
     paddingHorizontal: 2,
     fontFamily: "Figtree-Medium",
+  },
+  date_picker_item: {
+    borderWidth: 2,
+    borderRadius: 12,
   },
 
   main_chart: {
